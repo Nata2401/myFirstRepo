f7d803183cd57c148a4e3e02e3776c75d324dcaa
Nata2401 <kuzyan.rd@yandex.ru>
2021-11-25 18:23:01 +0300

    Исправлено

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var title;
-var screens;
-var screenPrice;
-var rollback;
-var fullPrice;
-var adaptive;
+const title = 1;
+const screens = 1;
+const screenPrice = 1;
+const rollback = 1;
+const fullPrice = 1;
+const adaptive = 1;
 
 
 alert("Hello");
