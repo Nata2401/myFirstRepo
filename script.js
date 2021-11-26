2131080bdc663c26b74c6ccab7f74c21c07a551a
Nata2401 <kuzyan.rd@yandex.ru>
2021-11-26 15:39:14 +0300

    Выполено второе задание

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-const title = 1;
-const screens = 1;
-const screenPrice = 1;
-const rollback = 1;
-const fullPrice = 1;
-const adaptive = 1;
+const title = 'Javascript';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 5;
+const rollback = 24;
+const fullPrice = 1000;
+const adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+
+console.log(screens.length);
+
+console.log(screens.toLowerCase().split(" "));
+
+console.log(`Процент отката посреднику за работу ${fullPrice*rollback/100} рублей`);
+
+
 
 
 alert("Hello");
